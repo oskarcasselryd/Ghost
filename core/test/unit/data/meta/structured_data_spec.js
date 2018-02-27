d547da2da1ab361ca3942ebbf56fca731eda32fb
Henry Bogaeus <henry.bogaeus@gmail.com>
2018-02-27 13:12:46 +0100

    Added test to improve coverage to 96%

--- core/test/unit/data/meta/structured_data_spec.js
+++ core/test/unit/data/meta/structured_data_spec.js
@@ -147,6 +147,70 @@ describe('getStructuredData', function () {
         done();
     });
 
+    it('should return structured data with og:description and twitter:description set as the excerpt if no description exists', function (done) {
+        var metadata = {
+            blog: {
+                title: 'Blog Title',
+                facebook: 'testuser',
+                twitter: '@testuser'
+            },
+            authorName: 'Test User',
+            ogType: 'article',
+            metaTitle: 'Post Title',
+            canonicalUrl: 'http://mysite.com/post/my-post-slug/',
+            publishedDate: '2015-12-25T05:35:01.234Z',
+            modifiedDate: '2016-01-21T22:13:05.412Z',
+            coverImage: {
+                url: 'http://mysite.com/content/image/mypostcoverimage.jpg',
+                dimensions: {
+                    width: 500,
+                    height: 500
+                }
+            },
+            ogImage: {
+                url: null
+            },
+            twitterImage: null,
+            ogTitle: '',
+            ogDescription: '',
+            twitterTitle: '',
+            twitterDescription: '',
+            authorFacebook: 'testpage',
+            creatorTwitter: '@twitterpage',
+            keywords: ['one', 'two', 'tag'],
+            metaDescription: 'Post meta description',
+            excerpt: 'Post excerpt'
+        },  structuredData = getStructuredData(metadata, coverageObject);
+
+        should.deepEqual(structuredData, {
+            'article:modified_time': '2016-01-21T22:13:05.412Z',
+            'article:published_time': '2015-12-25T05:35:01.234Z',
+            'article:tag': ['one', 'two', 'tag'],
+            'article:publisher': 'https://www.facebook.com/testuser',
+            'article:author': 'https://www.facebook.com/testpage',
+            'og:description': 'Post excerpt',
+            'og:image': 'http://mysite.com/content/image/mypostcoverimage.jpg',
+            'og:image:width': 500,
+            'og:image:height': 500,
+            'og:site_name': 'Blog Title',
+            'og:title': 'Post Title',
+            'og:type': 'article',
+            'og:url': 'http://mysite.com/post/my-post-slug/',
+            'twitter:card': 'summary_large_image',
+            'twitter:data1': 'Test User',
+            'twitter:data2': ['one', 'two', 'tag'].join(', '),
+            'twitter:description': 'Post excerpt',
+            'twitter:image': 'http://mysite.com/content/image/mypostcoverimage.jpg',
+            'twitter:label1': 'Written by',
+            'twitter:label2': 'Filed under',
+            'twitter:title': 'Post Title',
+            'twitter:url': 'http://mysite.com/post/my-post-slug/',
+            'twitter:site': '@testuser',
+            'twitter:creator': '@twitterpage'
+        });
+        done();
+    });
+
     it('should return structured data from metadata with no nulls', function (done) {
         var metadata = {
             blog: {
